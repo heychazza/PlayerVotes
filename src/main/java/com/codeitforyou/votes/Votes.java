4a69881cfc40c55d4a44a01ae1d13a76db152212
Chazza <me@chazza.net>
2020-04-05 18:10:04 +0100

    Initial commit

---
+++ src/main/java/com/codeitforyou/votes/Votes.java
@@ -0,0 +1,16 @@
+package com.codeitforyou.votes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Votes extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
